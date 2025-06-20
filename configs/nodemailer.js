0e6affffe21ecd201b478a27deb5c2d9e4b7d09e
fajri <fajri42vandi@gmail.com>
2025-06-20 14:32:57 +0700

    fix: bug

--- configs/nodemailer.js
+++ configs/nodemailer.js
@@ -3,7 +3,6 @@ import { createTransport } from "nodemailer";
 const email = process.env.USER_EMAIL;
 const emailPassword = process.env.USER_EMAIL_PASSWORD;
 const environment = process.env.NODE_ENV;
-console.log({ email, emailPassword, environment });
 const mailConfig = {
     host: process.env.HOST_MAIL,
     port: environment === "development" ? 587 : 465,
